9d4bdb54ef8769e53975a965e3c841092e420c08
Shawn Caeiro <shawncaeiro@gmail.com>
2016-03-24 13:02:05 -0500

    Normalized database

--- tune_throwback/models.py
+++ tune_throwback/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 import datetime
 
-class SongRank(models.Model):
-    song = models.TextField(default='')
+class Song(models.Model):
+    title = models.TextField(default='')
     artist = models.TextField(default='')
+
+    def __str__(self):
+        return "%s %s" % (self.title, self.artist)
+
+class Rank(models.Model):
     rank = models.IntegerField(default=101)
     week = models.DateField(default=datetime.date(1980,1,1))
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s %s %d %s" % (self.song.title, self.song.artist, self.rank, str(self.week))
 
 
 
--- tune_throwback/views.py
+++ tune_throwback/views.py
@@ -3,35 +3,33 @@ import sys
 
 from django.shortcuts import redirect, render
 
-from tune_throwback.models import SongRank
+from tune_throwback.models import Song, Rank
 
 
 def home_page(request):
-    songs = list(SongRank.objects.order_by()
-        .values_list('song', flat=True).distinct())
+    songs = list(Song.objects.values_list('title', flat=True).distinct())
     j_songs = json.dumps(songs)
     return render(request, 'home.html', {'songs':j_songs})
 
-def results_page_helper(request, song):
-    songs = list(SongRank.objects.order_by()
-        .values_list('song', flat=True).distinct())
+def results_page_helper(request, r_song):
+    songs = list(Song.objects.values_list('title', flat=True).distinct())
     j_songs = json.dumps(songs)
 
-    similar_songs = SongRank.objects.filter(week=song.week).order_by('rank')[:25]
-    return render(request, 'results.html', {'all_songs': j_songs, 'song': song, 'songs':similar_songs})
+    similar_songs = Rank.objects.filter(week=r_song.week).order_by('rank')[:25]
+    return render(request, 'results.html', {'all_songs': j_songs, 'ranked': r_song, 'songs':similar_songs})
 
 def results_page(request, song_id = None):
     if str(song_id).isdigit():
         try:
-            s_temp = SongRank.objects.get(pk=song_id)
-            s_ranked = SongRank.objects.filter(song=s_temp.song).order_by('rank')[0]
-            return results_page_helper(request, s_ranked)
+            s_temp = Song.objects.get(pk=song_id)
+            r_song = Rank.objects.filter(song__title = s_temp.title).order_by('rank')[0]
+            return results_page_helper(request, r_song)
         except:
             return redirect('/results')
     elif request.method == 'POST':
         try:
-            s = SongRank.objects.filter(song=request.POST['song_text']).order_by('rank')[0]
-            return results_page_helper(request, s)            
+            r_song = Rank.objects.filter(song__title= request.POST["song_text"]).order_by('rank')[0]
+            return results_page_helper(request, r_song)            
         except:
             return redirect('/')            
     else:
