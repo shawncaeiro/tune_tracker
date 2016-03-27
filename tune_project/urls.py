99bdb42d14a828db9a6feed676db8cc030937977
Shawn Caeiro <shawncaeiro@gmail.com>
2016-03-26 22:02:17 -0500

    Random button and url reconfiguration

--- tune_project/urls.py
+++ tune_project/urls.py
@@ -7,6 +7,7 @@ from tune_throwback import views
 
 urlpatterns = [
     url(r'results/(?P<song_id>\d+)/$', views.results_page, name='results_ided'),
+    url(r'results/random/', views.results_page_random, name='results'),
     url(r'results', views.results_page, name='results'),
     url(r'^admin/', admin.site.urls),
     url(r'$', views.home_page, name='home'),
--- tune_throwback/views.py
+++ tune_throwback/views.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import random
 
 from django.shortcuts import redirect, render
 
@@ -16,12 +17,24 @@ def home_page(request):
     return render(request, 'home.html', {'songs':j_songs})
 
 def results_page_helper(request, r_song):
-    songs = list(Song.objects.values_list('title', flat=True).distinct())
-    j_songs = json.dumps(songs)
+    songs = list(Song.objects.values_list('title', 'artist').distinct())
+    j_songs = []
+    for song in songs:
+        j_songs.append("{0} -- {1}".format(song[0],song[1]))
+    j_songs = list(set(i.lower().title() for i in j_songs))
+    j_songs = json.dumps(j_songs)
 
     similar_songs = Rank.objects.filter(week=r_song.week).order_by('rank')[:25]
     spotify_ids = Rank.objects.filter(week=r_song.week).exclude(song__spotify_id = "").order_by('rank')[:25]
     return render(request, 'results.html', {'all_songs': j_songs, 'ranked': r_song, 'songs':similar_songs, 'spotify_ids':spotify_ids})
+
+def results_page_random(request):
+    #song = random.choice(Song.objects.all())
+    #ranked_song = Rank.objects.filter(song__title = song.title, song__artist = song.artist).order_by('rank')[0]
+    ranked_song = random.choice(Rank.objects.filter(rank__lte = 25))
+    return redirect('/results/{0}/'.format(ranked_song.song.pk))
+
+#def results_page_post
 
 def results_page(request, song_id = None):
     if str(song_id).isdigit():
@@ -35,7 +48,7 @@ def results_page(request, song_id = None):
         try:
             s, a = request.POST["song_text"].split(' -- ')
             r_song = Rank.objects.filter(song__title__iexact= s, song__artist__iexact= a).order_by('rank')[0]
-            return results_page_helper(request, r_song)
+            return redirect('/results/{0}/'.format(r_song.song.pk))
         except:
             return redirect('/')            
     else:
